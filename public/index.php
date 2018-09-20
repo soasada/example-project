adb188091bee9641329a30b18c783043e794153c
Nicolas Vargas <nvortega92@gmail.com>
2018-09-20 21:06:06 +0200

    Removed vendor boost

--- public/index.php
+++ public/index.php
@@ -5,11 +5,7 @@ use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
 
-if ($_SERVER['APP_ENV'] === 'dev' || $_SERVER['APP_ENV'] === 'test') {
-    require '/home/vagrant/example-project/vendor/autoload.php';
-} else {
-    require __DIR__.'/../vendor/autoload.php';
-}
+require __DIR__.'/../vendor/autoload.php';
 
 // The check is to ensure we don't use .env in production
 if (!isset($_SERVER['APP_ENV'])) {
